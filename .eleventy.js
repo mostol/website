4d80c0c1aa8c88a7621b84e2582b1c7131825ee3
Jackson Mostoller <jax.mostoller@gmail.com>
2022-01-27 11:41:59 -0700

    Add syntax highlighting

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,11 @@
+// Enable syntax highlighting
+const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
+
 module.exports = function(eleventyConfig) {
+
+  // Add syntax highlighting
+  eleventyConfig.addPlugin(syntaxHighlight);
+
   // Copy `src/style.css` to `_site/style.css`
   eleventyConfig.addPassthroughCopy("src/style.css");
 
